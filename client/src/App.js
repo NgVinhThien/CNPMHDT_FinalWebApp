e9a7422112c32852eb27916eac5296c766422bf0
NguyenVinhThien <nguyennvinhthien@gmail.com>
2023-04-22 17:51:35 +0700

    add homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
-import Home from './components/Home'
+import Home from './views/Home'
 
 function App() {
   return <Router>
     <Routes>
-      <Route path='/' element= {<Home/>} />
+      <Route path='/' element= {<Home/>}/>
     </Routes>
     
     
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
@@ -8,12 +10,13 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 const Landing = () => {
   return (
     <div>
-        <Carousel>
+    <Carousel>
       <Carousel.Item>
         <img
           className="d-block w-100"
           src="https://th.bing.com/th/id/R.7f34a70a5bee79488309934920d316cb?rik=IRJI6pSFo6OI%2fQ&riu=http%3a%2f%2fallpicts.in%2fwp-content%2fuploads%2f2018%2f03%2fNatural-Images-HD-1080p-Download-with-Keyhole-Arch-at-Pfeiffer-Beach.jpg&ehk=JPI0MFI04N62Xtd7MT1P2sw4vJgwODLsp1EcpgvuGFo%3d&risl=&pid=ImgRaw&r=0"
           alt="First slide"
+          style={{height: 250}}
         />
         <Carousel.Caption>
           <h3>First slide label</h3>
@@ -25,6 +28,7 @@ const Landing = () => {
           className="d-block w-100"
           src="https://couchclarity.com/wp-content/uploads/2015/10/sample-slide5.jpg"
           alt="Second slide"
+          style={{height: 250}}
         />
 
         <Carousel.Caption>
@@ -37,6 +41,7 @@ const Landing = () => {
           className="d-block w-100"
           src="https://th.bing.com/th/id/OIP.wwxK07x0Umfnh0l-nrjxjgHaDg?pid=ImgDet&rs=1"
           alt="Third slide"
+          style={{height: 250}}
         />
         <Carousel.Caption>
           <h3>Third slide label</h3>
@@ -47,32 +52,44 @@ const Landing = () => {
       </Carousel.Item>
     </Carousel>
     <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">Link</Nav.Link>
+            <NavDropdown title="Link" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+              <NavDropdown.Item href="#action5">
+                Something else here
               </NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link href="#" disabled>
+              Link
+            </Nav.Link>
           </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
     </div>
-    
-    
-
   );
 }
 
