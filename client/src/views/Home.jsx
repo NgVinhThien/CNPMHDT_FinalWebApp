d4b03cad404f5a33e32a78d8effaa1192589c03c
NguyenVinhThien <nguyennvinhthien@gmail.com>
2023-04-30 10:58:08 +0700

    edit homepage

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react';
 import Header from '../components/Header'
 import '../_CSS/Home.css'
 import '../_CSS/verticalCard.css'
-import Stack from 'react-bootstrap/Stack'
-import Card from 'react-bootstrap/Card';
+import '../_CSS/horizontalCard.css'
+// import Stack from 'react-bootstrap/Stack'
+// import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image'
 import axios from 'axios'
 
@@ -38,62 +39,29 @@ export default function Home() {
                           <span>Tác giả</span>
                           <span>{item.publish_date}</span>
                       </div>
-                  </div>
-                  
+                  </div>  
               </div>))}
               </div>
             </div>
-            <div class="largeTitle">Tin mới</div>
-            {/* <div class= "groupCard">
-              <Stack direction="horizontal" gap={2} style={{height: '110px', marginTop: '10px'}}>
-                    <Card.Img src="https://th.bing.com/th/id/OIP.iSu2RcCcdm78xbxNDJMJSgHaEo?pid=ImgDet&rs=1" style={{ width: '20%', height: '100%' } } />
-                    <Card.Body style={{ width: '100%', height: '100%' } }>
-                      <Card.Title >Card Title</Card.Title>
-                      <Card.Text style={{margin: 0, padding:0}}>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                      </Card.Text>
-                      <Card.Text style={{margin: 0, padding:0}}>Tác giả: </Card.Text>
-                      <Card.Text style={{margin: 0, padding:0}}>Ngày đăng: </Card.Text>
-                    </Card.Body>
-                  </Stack>
-              <Stack direction="horizontal" gap={2} style={{height: '110px', marginTop: '10px'}}>
-                    <Card.Img src="https://th.bing.com/th/id/OIP.iSu2RcCcdm78xbxNDJMJSgHaEo?pid=ImgDet&rs=1" style={{ width: '20%', height: '100%' } } />
-                    <Card.Body style={{ width: '100%', height: '100%' } }>
-                      <Card.Title >Card Title</Card.Title>
-                      <Card.Text style={{margin: 0, padding:0}}>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                      </Card.Text>
-                      <Card.Text style={{margin: 0, padding:0}}>Tác giả: </Card.Text>
-                      <Card.Text style={{margin: 0, padding:0}}>Ngày đăng: </Card.Text>
-                    </Card.Body>
-                  </Stack>
-              <Stack direction="horizontal" gap={2} style={{height: '110px', marginTop: '10px'}}>
-                    <Card.Img src="https://th.bing.com/th/id/OIP.iSu2RcCcdm78xbxNDJMJSgHaEo?pid=ImgDet&rs=1" style={{ width: '20%', height: '100%' } } />
-                    <Card.Body style={{ width: '100%', height: '100%' } }>
-                      <Card.Title >Card Title</Card.Title>
-                      <Card.Text style={{margin: 0, padding:0}}>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                      </Card.Text>
-                      <Card.Text style={{margin: 0, padding:0}}>Tác giả: </Card.Text>
-                      <Card.Text style={{margin: 0, padding:0}}>Ngày đăng: </Card.Text>
-                    </Card.Body>
-                  </Stack>
-              <Stack direction="horizontal" gap={2} style={{height: '110px', marginTop: '10px'}}>
-                    <Card.Img src="https://th.bing.com/th/id/OIP.iSu2RcCcdm78xbxNDJMJSgHaEo?pid=ImgDet&rs=1" style={{ width: '20%', height: '100%' } } />
-                    <Card.Body style={{ width: '100%', height: '100%' } }>
-                      <Card.Title >Card Title</Card.Title>
-                      <Card.Text style={{margin: 0, padding:0}}>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                      </Card.Text>
-                      <Card.Text style={{margin: 0, padding:0}}>Tác giả: </Card.Text>
-                      <Card.Text style={{margin: 0, padding:0}}>Ngày đăng: </Card.Text>
-                    </Card.Body>
-                  </Stack>
-            </div> */}
+            <div class="tinmoi">
+              <div class="largeTitle">Tin mới</div>
+              {data.map(item=>(
+                 <div class= "horizontal-card">
+                 <img class="horizontal-card-image" src="https://th.bing.com/th/id/OIP.iSu2RcCcdm78xbxNDJMJSgHaEo?pid=ImgDet&rs=1"></img>
+                 <div class="horizontal-card-body">
+                 <div class= "card-title">{item.title}</div>
+                 <div class="card-description">{item.abstract}</div>
+                       <div class="article-info">
+                           <span>Tác giả</span>
+                           <span>{item.publish_date}</span>
+                       </div>
+                 </div>
+               </div>
+              ))}
+             
+              
+            </div>
+            
         </div>
         <div class= "right-home">
           <Image class="image-of-right-home" style={{width: '100%', margin: 0, padding: 0}} src="https://th.bing.com/th/id/OIP.iSu2RcCcdm78xbxNDJMJSgHaEo?pid=ImgDet&rs=1"/>
