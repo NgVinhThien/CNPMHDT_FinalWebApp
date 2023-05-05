34154c70679572e00ce65ce2a6be0de1f4cb1643
DucTrungDo <ducdt2192@gmail.com>
2023-05-05 08:50:48 +0700

    commit

--- client/src/views/DangKy.jsx
+++ client/src/views/DangKy.jsx
@@ -1,105 +1,47 @@
 import React, { useState } from 'react';
 import $ from 'jquery';
-import 'jquery-datetimepicker';
-import 'jquery-datetimepicker/build/jquery.datetimepicker.min.css';
+import DateTimePicker from 'react-datetime-picker';
 import './css/DangKy.css'
 
-function Register() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleConfirmPasswordChange = (event) => {
-    setConfirmPassword(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    $.getJSON(`/Account/IsAvailable?user=${username}`, function (data) {
-      if (data === true) {
-        $('#frmRegister').off('submit').submit();
-      } else {
-        alert('Tên tài khoản đã được sử dụng');
-      }
-    });
-  };
-
-  const handleDateChange = () => {
-    $('#txtDOB').datetimepicker({
-      format: 'd/m/Y',
-      timepicker: false,
-      mask: true
-    });
-  };
-
-  const handleSelect = (event) => {
-    console.log(event.target.value);
-  };
-
-  const handleFormSubmit = () => {
-    if (password !== confirmPassword) {
-      setErrorMessage('The password and confirm password fields do not match');
-      return false;
-    } else {
-      setErrorMessage('');
-      return true;
-    }
-  };
-
+function DangKy() {
   return (
-    <div className="form-container">
-      <h1>Đăng Ký</h1>
-      <form id="frmRegister" onSubmit={handleSubmit}>
-        <label htmlFor="txtUsername">Username</label>
-        <input type="text" id="txtUsername" name="username" placeholder="Nhập tài khoản" value={username} onChange={handleUsernameChange} required />
-        <label htmlFor="txtName">Họ và Tên</label>
-        <input type="text" id="txtName" name="name" placeholder="Nhập họ tên" required />
-        <label htmlFor="txtEmail">Email</label>
-        <input type="email" id="txtEmail" name="email" placeholder="Nhập email" required />
-        <label htmlFor="txtDOB">Ngày sinh</label>
-        <input type="text" id="txtDOB" name="dob" placeholder="Nhập ngày sinh" onChange={handleDateChange} required />
-        <label htmlFor="txtPassword">Mật khẩu</label>
-        <input type="password" id="txtPassword" name="rawpwd" placeholder="Nhập mật khẩu" value={password} onChange={handlePasswordChange} required />
-        <label htmlFor="txtConfirm">Xác nhận mật khẩu</label>
-        <input type="password" id="txtConfirm" placeholder="Xác nhận mật khẩu" value={confirmPassword} onChange={handleConfirmPasswordChange} required />
-        <label>Vai trò</label>
-        <select style={{
-          width: '100%',
-          padding: '12px 20px',
-          color: '#555',
-          marginBottom: '32px',
-          boxSizing: 'border-box',
-          border: '2px solid #ccc',
-          borderRadius: '4px',
-          fontSize: '16px'
-        }} className="selectRole" name="role" onChange={handleSelect}>
-          <option value="0" selected>Subscriber</option>
-          <option value="1">Writer</option>
-            </select>
-            <div className="terms">
-              Khi bấm tạo tài khoản bạn đã đồng ý với
-              <a href="#" className="rule">
-                quy định
-              </a>
-              của tòa soạn
-            </div>
-            <div id="password-error" className="error"></div>
-            <button type="submit" onClick={handleFormSubmit}>Tạo tài khoản</button>
-          </form>
-        </div>
-        
-    );
+    <div>
+      <meta charSet="UTF-8" />
+      <title>Đăng ký</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-datetimepicker/2.5.20/jquery.datetimepicker.min.css" integrity="sha512-f0tzWhCwVFS3WeYaofoLWkTP62ObhewQ1EZn65oSYDZUg1+CyywGKkWzm8BxaJj5HGKI72PnMH9jYyIFz+GH7g==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      <div className="form-container">
+        <h1>Đăng Ký</h1>
+        <form action method="POST" id="frmRegister" onsubmit="return checkPassword();">
+          <label htmlFor="txtUsername">Username</label>
+          <input type="text" id="txtUsername" name="username" placeholder="Nhập tài khoản" required />
+          <label htmlFor="txtName">Họ và Tên</label>
+          <input type="text" id="txtName" name="name" placeholder="Nhập họ tên" required />
+          <label htmlFor="txtEmail">Email</label>
+          <input type="email" id="txtEmail" name="email" placeholder="Nhập email" required />
+          <label htmlFor="txtDOB">Ngày sinh</label>
+          <input type="text" id="txtDOB" name="dob" placeholder="Nhập ngày sinh" required />
+          <label htmlFor="txtPassword">Mật khẩu</label>
+          <input type="password" id="txtPassword" name="rawpwd" placeholder="Nhập mật khẩu" required />
+          <label htmlFor="txtConfirm">Xác nhận mật khẩu</label>
+          <input type="password" id="txtConfirm" placeholder="Xác nhận mật khẩu" required />
+          <label>Vai trò</label>
+          <select style={{ width: '100%', padding: '12px 20px', color: '#555', marginBottom: '32px', boxSizing: 'border-box', border: '2px solid #ccc', borderRadius: '4px', fontSize: '16px' }} className="selectRole" name="role">
+            <option value={0} selected>Subscriber</option>
+            <option value={1}>Writer</option>
+          </select>
+          <div className="terms">
+            Khi bấm tạo tài khoản bạn đã đồng ý với&nbsp;
+            <a href="#" className="rule">quy định</a>
+            &nbsp;của tòa soạn
+          </div>
+          <div id="password-error" className="error" />
+          <button type="submit">Tạo tài khoản</button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
-export default Register;
+export default DangKy;
 
--- client/src/views/DangNhap.jsx
+++ client/src/views/DangNhap.jsx
@@ -1,61 +1,53 @@
-import React from "react";
+import react from 'react';
 import './css/DangNhap.css'
+document.body.style.backgroundColor = "#f1f1f1";
 
-
-function Login(props) {
-  const hasError = props.hasError;
-  const errorMessage = props.errorMessage;
-
+function DangNhap() {
   return (
-    <div className="login-container">
-      <h1>Đăng nhập</h1>
-      {hasError && (
-        <div className="alert" role="alert">
-          <strong>Login failed!</strong> {errorMessage}
-        </div>
-      )}
-      <form action="" method="POST" id="frmLogin">
-        <div className="form-group">
-          <label htmlFor="txtUsername">Tài khoản</label>
-          <input type="text" id="txtUsername" name="username" placeholder="Nhập tài khoản" required />
-        </div>
-        <div className="form-group">
-          <label htmlFor="txtPassword">Mật khẩu</label>
-          <input type="password" id="txtPassword" name="password" placeholder="Nhập mật khẩu" required />
-        </div>
-        <div className="forgot_change-password">
-          {/* Phần quên mật khẩu */}
-          <div className="form-group">
-            <a href="#" className="forgot-password">Quên mật khẩu</a>
+    <div>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <title>Đăng nhập</title>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossOrigin="anonymous" />
+      <div className="login-container">
+        <h1>Đăng nhập</h1>
+        {/* <c:if test="${hasError}">
+          <div className="alert" role="alert">
+            <strong>Login failed!</strong> ${'{'}errorMessage{'}'}
           </div>
-          {/* Phần đổi mật khẩu */}
+        </c:if> */}
+        <form action method="POST" id="frmLogin">
           <div className="form-group">
-            <a href="#" className="forgot-password">Đổi mật khẩu</a>
+            <label htmlFor="txtUsername">Tài khoản</label>
+            <input type="text" id="txtUsername" name="username" placeholder="Nhập tài khoản" required />
           </div>
-        </div>
-        <button type="submit" className="btn btn-primary btn-block">
-          Đăng nhập
-        </button>
-
-        <div className="form-group">
-          <a href={`${props.contextPath}/Account/DangKy`} className="signup-link">
-            Chưa có tài khoản? Đăng ký ngay
-          </a>
-        </div>
-        <div className="or-separator">
-          <span>hoặc</span>
-        </div>
-        <button className="btnn facebook" data-provider="facebook">
-          <i className="fab fa-facebook-f"></i>
-          <span>Đăng nhập bằng Facebook</span>
-        </button>
-        <button className="btnn google" data-provider="google">
-          <i className="fab fa-google"></i>
-          <span>Đăng nhập bằng Google</span>
-        </button>
-      </form>
+          <div className="form-group">
+            <label htmlFor="txtPassword">Mật khẩu</label>
+            <input type="password" id="txtPassword" name="password" placeholder="Nhập mật khẩu" required />
+          </div>
+          <div className="forgot_change-password">
+            {/* Phần quên mật khẩu */}
+            <div className="form-group">
+              <a href="#" className="forgot-password">Quên mật khẩu</a>
+            </div>
+            {/* Phần đổi mật khẩu */}
+            <div className="form-group">
+              <a href="#" className="forgot-password">Đổi mật khẩu</a>
+            </div>
+          </div>
+          <button type="submit" className="btn-primary">Đăng nhập</button>
+          <div className="form-group">
+            <a href="#" className="signup-link">Chưa có tài khoản? Đăng ký ngay</a>
+          </div>
+          <div className="or-separator">
+            <span>hoặc</span>
+          </div>
+          <button className="btnn facebook" data-provider="facebook"><i className="fab fa-facebook-f" /><span>Đăng nhập bằng Facebook</span></button>
+          <button className="btnn google" data-provider="google"><i className="fab fa-google" /><span>Đăng nhập bằng Google</span></button>
+        </form>
+      </div>
     </div>
   );
 }
 
-export default Login;
+export default DangNhap;
